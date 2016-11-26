48d71dc83f56747b78709eb54f03425bb98237bd
antham <hamonanth@gmail.com>
2016-11-26 21:07:35 +0100

    test(expander_test) : add

--- chyle/expander_test.go
+++ chyle/expander_test.go
@@ -45,6 +45,36 @@ func TestJiraExpander(t *testing.T) {
 	assert.True(t, gock.IsDone(), "Must have no pending requests")
 }
 
+func TestJiraExpanderWithNoJiraIssueIdDefined(t *testing.T) {
+	defer gock.Off()
+
+	gock.New("http://test.com/rest/api/2/issue/10000").
+		Reply(200).
+		BodyString(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
+
+	client := &http.Client{Transport: &http.Transport{}}
+	gock.InterceptClient(client)
+
+	jiraClient, err := jira.NewClient(client, "http://test.com")
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	j := JiraIssueExpander{jiraClient}
+
+	result, err := j.Expand(&map[string]interface{}{"test": "test"})
+
+	expected := map[string]interface{}{
+		"test": "test",
+	}
+
+	assert.NoError(t, err, "Must return no errors")
+	assert.Equal(t, expected, *result, "Must return same struct than the one submitted")
+	assert.False(t, gock.IsDone(), "Must have one pending request")
+
+}
+
 func TestExpander(t *testing.T) {
 	defer gock.Off()
 
