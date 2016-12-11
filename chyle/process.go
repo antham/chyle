73a72123b4a77ca22798e2657539273c8eced88a
antham <hamonanth@gmail.com>
2016-12-11 01:34:38 +0100

    feat(chyle/process) : add process
    * process object is created from configuration
    * process is apply then against commit set using proceed function

---
+++ chyle/process.go
@@ -0,0 +1,67 @@
+package chyle
+
+import (
+	"github.com/spf13/viper"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+// process represents all configuration operations defined
+// needed to create a changelog
+type process struct {
+	matchers   *[]Matcher
+	extractors *[]Extracter
+	expanders  *[]Expander
+	senders    *[]Sender
+}
+
+// buildProcess creates process entity from defined configuration
+func buildProcess(viper *viper.Viper) (*process, error) {
+	m, err := CreateMatchers(viper.GetStringMapString("matchers"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	ext, err := CreateExtractors(viper.GetStringMap("extractors"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	exp, err := CreateExpanders(viper.GetStringMap("expanders"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := CreateSenders(viper.GetStringMap("senders"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &process{
+		m,
+		ext,
+		exp,
+		s,
+	}, nil
+}
+
+// proceed extracts datas from a set of commits
+func proceed(process *process, commits *[]git.Commit) error {
+	comExt, err := Extract(process.extractors, TransformCommitsToMap(Filter(process.matchers, commits)))
+
+	if err != nil {
+
+		return err
+	}
+
+	comExp, err := Expand(process.expanders, comExt)
+
+	if err != nil {
+		return err
+	}
+
+	return Send(process.senders, comExp)
+}
