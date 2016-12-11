a677e1eea8b819a3e0a3c4314c457c580b322444
antham <hamonanth@gmail.com>
2016-12-11 01:36:21 +0100

    feat(chyle/git) : add git
    * resolveRef retrieves hash commit from a reference
    * fetchCommits retrieves commits from a reference range

---
+++ chyle/git.go
@@ -0,0 +1,83 @@
+package chyle
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+)
+
+// resolveRef give hash commit for a given string reference
+func resolveRef(refCommit string, repository *git.Repository) (*git.Commit, error) {
+	hash := plumbing.Hash{}
+
+	if strings.ToLower(refCommit) == "head" {
+		head, err := repository.Head()
+
+		if err == nil {
+			return repository.Commit(head.Hash())
+		}
+	}
+
+	iter, err := repository.Refs()
+
+	if err != nil {
+		return &git.Commit{}, err
+	}
+
+	err = iter.ForEach(func(ref *plumbing.Reference) error {
+		if ref.Name().Short() == refCommit {
+			hash = ref.Hash()
+		}
+
+		return nil
+	})
+
+	if err == nil && !hash.IsZero() {
+		return repository.Commit(hash)
+	}
+
+	hash = plumbing.NewHash(refCommit)
+
+	if !hash.IsZero() {
+		return repository.Commit(hash)
+	}
+
+	return &git.Commit{}, fmt.Errorf(`Can't find reference "%s"`, refCommit)
+}
+
+// fetchCommits retrieves commits between a reference range
+func fetchCommits(repoPath string, fromRef string, toRef string) (*[]git.Commit, error) {
+	commits := []git.Commit{}
+	repo, err := git.NewFilesystemRepository(repoPath + "/.git/")
+
+	if err != nil {
+		return nil, err
+	}
+
+	fromCommit, err := resolveRef(fromRef, repo)
+
+	if err != nil {
+		return &[]git.Commit{}, err
+	}
+
+	toCommit, err := resolveRef(toRef, repo)
+
+	if err != nil {
+		return &[]git.Commit{}, err
+	}
+
+	_ = git.WalkCommitHistory(toCommit, func(c *git.Commit) error {
+		commits = append(commits, *c)
+
+		if c.ID() == fromCommit.ID() {
+			return io.EOF
+		}
+
+		return nil
+	})
+
+	return &commits, nil
+}
