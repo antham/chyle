f09bf5cb3cccf2b068330ffd6c69d60ab2b01b62
antham <hamonanth@gmail.com>
2017-02-05 22:53:30 +0100

    ref(chyle/sender) : move fixture to fixture folder

--- chyle/sender_github_release_test.go
+++ chyle/sender_github_release_test.go
@@ -1,6 +1,7 @@
 package chyle
 
 import (
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -13,6 +14,10 @@ import (
 func TestGithubReleaseSender(t *testing.T) {
 	defer gock.Off()
 
+	tagCreationResponse, err := ioutil.ReadFile("fixtures/1-github-tag-creation-response.json")
+
+	assert.NoError(t, err, "Must read json fixture file")
+
 	gock.New("https://api.github.com").
 		Post("/repos/test/test/releases").
 		MatchHeader("Authorization", "token d41d8cd98f00b204e9800998ecf8427e").
@@ -20,45 +25,7 @@ func TestGithubReleaseSender(t *testing.T) {
 		HeaderPresent("Accept").
 		JSON(GithubRelease{TagName: "v1.0.0", Name: "TEST", Body: "Hello world !"}).
 		Reply(201).
-		JSON(`{
-  "url": "https://api.github.com/repos/test/test/releases/1",
-  "html_url": "https://github.com/test/test/releases/v1.0.0",
-  "assets_url": "https://api.github.com/repos/test/test/releases/1/assets",
-  "upload_url": "https://uploads.github.com/repos/test/test/releases/1/assets{?name,label}",
-  "tarball_url": "https://api.github.com/repos/test/test/tarball/v1.0.0",
-  "zipball_url": "https://api.github.com/repos/test/test/zipball/v1.0.0",
-  "id": 1,
-  "tag_name": "v1.0.0",
-  "target_commitish": "master",
-  "name": "v1.0.0",
-  "body": "Description of the release",
-  "draft": false,
-  "prerelease": false,
-  "created_at": "2013-02-27T19:35:32Z",
-  "published_at": "2013-02-27T19:35:32Z",
-  "author": {
-    "login": "test",
-    "id": 1,
-    "avatar_url": "https://github.com/images/error/test_happy.gif",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/test",
-    "html_url": "https://github.com/test",
-    "followers_url": "https://api.github.com/users/test/followers",
-    "following_url": "https://api.github.com/users/test/following{/other_user}",
-    "gists_url": "https://api.github.com/users/test/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/test/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/test/subscriptions",
-    "organizations_url": "https://api.github.com/users/test/orgs",
-    "repos_url": "https://api.github.com/users/test/repos",
-    "events_url": "https://api.github.com/users/test/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/test/received_events",
-    "type": "User",
-    "site_admin": false
-  },
-  "assets": [
-
-  ]
-}{"tag_name": "v1.0.0","target_commitish": "master","name": "v1.0.0","body": "Description of the release","draft": false,"prerelease": false}`)
+		JSON(string(tagCreationResponse))
 
 	client := &http.Client{Transport: &http.Transport{}}
 	gock.InterceptClient(client)
