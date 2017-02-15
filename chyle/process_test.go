2f38d854b30dd0dac0571992601b809fb10453fe
Anthony HAMON <anthony.hamon@iadvize.com>
2017-02-15 13:05:17 +0100

    test(chyle/process) : add tests

---
+++ chyle/process_test.go
@@ -0,0 +1,67 @@
+package chyle
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/antham/envh"
+)
+
+func TestBuildProcessWithAnEmptyConfig(t *testing.T) {
+	config, err := envh.NewEnvTree("WHATEVER", "")
+
+	assert.NoError(t, err, "Must produces no errors")
+
+	_, err = buildProcess(&config)
+
+	assert.NoError(t, err, "Must produces no errors")
+}
+
+func TestBuildProcessWithWrongEnvironmentsVariables(t *testing.T) {
+	type g struct {
+		f func()
+		e string
+	}
+
+	tests := []g{
+		g{
+			func() {
+				setenv("CHYLE_MATCHERS_WHATEVER", "test")
+			},
+			`a wrong matcher key containing "WHATEVER" was defined`,
+		},
+		g{
+			func() {
+				setenv("CHYLE_EXTRACTORS_WHATEVER", "test")
+			},
+			`An environment variable suffixed with "ORIGKEY" must be defined with "WHATEVER", like EXTRACTORS_WHATEVER_ORIGKEY`,
+		},
+		g{
+			func() {
+				setenv("CHYLE_EXPANDERS_WHATEVER", "test")
+			},
+			`a wrong expander key containing "WHATEVER" was defined`,
+		},
+		g{
+			func() {
+				setenv("CHYLE_SENDERS_WHATEVER", "test")
+			},
+			`a wrong sender key containing "WHATEVER" was defined`,
+		},
+	}
+
+	for _, test := range tests {
+		restoreEnvs()
+
+		test.f()
+
+		config, err := envh.NewEnvTree("CHYLE", "_")
+
+		assert.NoError(t, err, "Must produces no errors")
+
+		_, err = buildProcess(&config)
+
+		assert.EqualError(t, err, test.e, "Must returns an error")
+	}
+}
