95d5ff5dad65e3f0f017ab7636f5bddd5ef856a3
antham <hamonanth@gmail.com>
2017-02-27 22:18:19 +0100

    ref(chyle/template) : move error to template, remove file

--- chyle/template.go
+++ chyle/template.go
@@ -2,8 +2,20 @@ package chyle
 
 import (
 	"bytes"
+	"fmt"
 	tmpl "html/template"
 )
+
+// ErrTemplateMalformed issued when something wrong
+// happened when creation a template
+type ErrTemplateMalformed struct {
+	err error
+}
+
+// Error return string error
+func (e ErrTemplateMalformed) Error() string {
+	return fmt.Sprintf("check your template is well-formed : %s", e.err.Error())
+}
 
 func populateTemplate(ID string, template string, data interface{}) (string, error) {
 	t := tmpl.New(ID)
