93fe666ceba41838bc94aca129ee3fd7fa993c68
antham <hamonanth@gmail.com>
2017-02-27 22:29:03 +0100

    ref(chyle/helper) : add config_validator file
    * move helpers related to validation to this new file

--- chyle/helper.go
+++ chyle/helper.go
@@ -1,79 +1,14 @@
 package chyle
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strings"
-
-	"github.com/antham/envh"
 )
-
-type strConfigMapping struct {
-	keyChain  []string
-	dstValue  *string
-	mandatory bool
-}
-
-type boolConfigMapping struct {
-	keyChain  []string
-	dstValue  *bool
-	mandatory bool
-}
 
 var logger *log.Logger
 
 func init() {
 	logger = log.New(os.Stdout, "CHYLE - ", log.Ldate|log.Ltime)
-}
-
-func extractStringConfig(config *envh.EnvTree, mapping []strConfigMapping, prefix []string) error {
-	var fullKey, v string
-	var err error
-
-	for _, e := range mapping {
-		if !e.mandatory && !config.IsExistingSubTree(e.keyChain...) {
-			continue
-		}
-
-		fullKey = strings.Join(append(prefix, e.keyChain...), "_")
-		v, err = config.FindString(e.keyChain...)
-
-		if err != nil {
-			return fmt.Errorf(`missing "%s"`, fullKey)
-		}
-
-		debug(`"%s" defined with value "%s"`, fullKey, v)
-
-		*(e.dstValue) = v
-	}
-
-	return nil
-}
-
-func extractBoolConfig(config *envh.EnvTree, mapping []boolConfigMapping, prefix []string) error {
-	var fullKey string
-	var v bool
-	var err error
-
-	for _, e := range mapping {
-		if !e.mandatory && !config.IsExistingSubTree(e.keyChain...) {
-			continue
-		}
-
-		fullKey = strings.Join(append(prefix, e.keyChain...), "_")
-		v, err = config.FindBool(e.keyChain...)
-
-		if err != nil {
-			return fmt.Errorf(`missing "%s"`, fullKey)
-		}
-
-		debug(`"%s" defined with value "%s"`, fullKey, v)
-
-		*(e.dstValue) = v
-	}
-
-	return nil
 }
 
 func debug(format string, v ...interface{}) {
