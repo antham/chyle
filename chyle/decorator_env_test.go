a23c2bf48d72c30c9ea67dbf4e8fbc8fcf1127cb
antham <hamonanth@gmail.com>
2017-05-03 21:25:30 +0200

    test(chyle/decorator_env) : rewrite tests

--- chyle/decorator_env_test.go
+++ chyle/decorator_env_test.go
@@ -10,10 +10,12 @@ func TestEnvDecorator(t *testing.T) {
 	setenv("DECORATORS_ENV_WHATEVER_VALUE", "hello world !")
 	setenv("DECORATORS_ENV_WHATEVER_DESTKEY", "helloWorld")
 
+	chyleConfig.DECORATORS.ENV = map[string]map[string]string{"WHATEVER": {"VALUE": "DECORATORS_ENV_WHATEVER_VALUE", "DESTKEY": "helloWorld"}}
+
 	metadatas := map[string]interface{}{}
 
-	e := envDecorator{"DECORATORS_ENV_WHATEVER_VALUE", "helloWorld"}
-	m, err := e.decorate(&metadatas)
+	e := buildEnvDecorators()
+	m, err := e[0].decorate(&metadatas)
 
 	assert.NoError(t, err, "Must returns no errors")
 	assert.Equal(t, map[string]interface{}{"helloWorld": "hello world !"}, *m, "Must dump environment variable in given destination key")
