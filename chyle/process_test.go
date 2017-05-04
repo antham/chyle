4e364840aadd97aba79998f02b4c659e11364727
antham <hamonanth@gmail.com>
2017-05-04 10:42:09 +0200

    test(chyle/process) : add test

--- chyle/process_test.go
+++ chyle/process_test.go
@@ -1,6 +1,7 @@
 package chyle
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,5 +20,37 @@ func TestBuildProcessWithAnEmptyConfig(t *testing.T) {
 	}
 
 	assert.EqualValues(t, expected, *p)
+}
 
+func TestBuildProcessWithAFullConfig(t *testing.T) {
+	chyleConfig = CHYLE{}
+
+	chyleConfig.FEATURES.HASMATCHERS = true
+	chyleConfig.MATCHERS = map[string]string{"TYPE": "merge"}
+	chyleConfig.FEATURES.HASEXTRACTORS = true
+	chyleConfig.EXTRACTORS = map[string]map[string]string{"TEST": {"TEST": "test"}}
+	chyleConfig.FEATURES.HASDECORATORS = true
+	chyleConfig.DECORATORS.ENV = map[string]map[string]string{"TEST": {"TEST": "test"}}
+	chyleConfig.FEATURES.HASSTDOUTSENDER = true
+	chyleConfig.SENDERS.STDOUT.FORMAT = "json"
+
+	p := buildProcess()
+
+	expected := process{
+		&[]matcher{
+			mergeCommitMatcher{},
+		},
+		&[]extracter{
+			regexpExtractor{
+				re: regexp.MustCompile(""),
+			},
+		},
+		&map[string][]decorater{
+			"datas":     {},
+			"metadatas": {},
+		},
+		&[]sender{},
+	}
+
+	assert.Equal(t, expected, *p)
 }
