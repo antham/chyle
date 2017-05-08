8703bcbff2dfee549969e00b89eb6459299e486d
antham <hamonanth@gmail.com>
2017-05-08 14:39:46 +0200

    test(chyle/config) : add regular cases

--- chyle/config_test.go
+++ chyle/config_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestResolveConfig(t *testing.T) {
+func TestResolveConfigWithErrors(t *testing.T) {
 	type g struct {
 		f func()
 		e string
@@ -390,6 +390,7 @@ func TestResolveConfig(t *testing.T) {
 			`provide a valid template string for "CHYLE_SENDERS_STDOUT_TEMPLATE" : "template: test:1: unexpected <.> in operand", "{{.....}}" given`,
 		},
 	}
+
 	for i, test := range tests {
 		restoreEnvs()
 		test.f()
@@ -404,6 +405,237 @@ func TestResolveConfig(t *testing.T) {
 
 		assert.Error(t, err, errDetail)
 		assert.EqualError(t, err, test.e, errDetail)
+	}
+}
+
+func TestResolveConfig(t *testing.T) {
+	type g struct {
+		f func()
+		c func() CHYLE
+	}
+
+	tests := []g{
+		// Mandatory parameters
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+
+				return c
+			},
+		},
+		// Matchers
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_MATCHERS_TYPE", "regular")
+				setenv("CHYLE_MATCHERS_AUTHOR", ".*")
+				setenv("CHYLE_MATCHERS_COMMITTER", ".*")
+				setenv("CHYLE_MATCHERS_MESSAGE", ".*")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASMATCHERS = true
+				c.MATCHERS = map[string]string{
+					"TYPE":      "regular",
+					"AUTHOR":    ".*",
+					"COMMITTER": ".*",
+					"MESSAGE":   ".*",
+				}
+
+				return c
+			},
+		},
+		// Extractors
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_EXTRACTORS_TEST_ORIGKEY", "test")
+				setenv("CHYLE_EXTRACTORS_TEST_DESTKEY", "test")
+				setenv("CHYLE_EXTRACTORS_TEST_REG", ".*")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASEXTRACTORS = true
+				c.EXTRACTORS = map[string]map[string]string{
+					"TEST": {
+						"ORIGKEY": "test",
+						"DESTKEY": "test",
+						"REG":     ".*",
+					},
+				}
+
+				return c
+			},
+		},
+		// Decorators env
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_DECORATORS_ENV_TEST_VARNAME", "var")
+				setenv("CHYLE_DECORATORS_ENV_TEST_DESTKEY", "destkey")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASDECORATORS = true
+				c.FEATURES.HASENVDECORATOR = true
+				c.DECORATORS.ENV = map[string]map[string]string{
+					"TEST": {
+						"DESTKEY": "destkey",
+						"VARNAME": "var",
+					},
+				}
+
+				return c
+			},
+		},
+		// Decorator jira
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_DECORATORS_JIRA_CREDENTIALS_URL", "http://test.com")
+				setenv("CHYLE_DECORATORS_JIRA_CREDENTIALS_USERNAME", "test")
+				setenv("CHYLE_DECORATORS_JIRA_CREDENTIALS_PASSWORD", "password")
+				setenv("CHYLE_DECORATORS_JIRA_KEYS_TEST_DESTKEY", "destkey")
+				setenv("CHYLE_DECORATORS_JIRA_KEYS_TEST_FIELD", "field")
+				setenv("CHYLE_EXTRACTORS_JIRAISSUEID_ORIGKEY", "test")
+				setenv("CHYLE_EXTRACTORS_JIRAISSUEID_DESTKEY", "test")
+				setenv("CHYLE_EXTRACTORS_JIRAISSUEID_REG", ".*")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASEXTRACTORS = true
+				c.FEATURES.HASDECORATORS = true
+				c.FEATURES.HASJIRADECORATOR = true
+				c.EXTRACTORS = map[string]map[string]string{
+					"JIRAISSUEID": {
+						"ORIGKEY": "test",
+						"DESTKEY": "test",
+						"REG":     ".*",
+					},
+				}
+				c.DECORATORS.JIRA.CREDENTIALS.URL = "http://test.com"
+				c.DECORATORS.JIRA.CREDENTIALS.USERNAME = "test"
+				c.DECORATORS.JIRA.CREDENTIALS.PASSWORD = "password"
+				c.DECORATORS.JIRA.KEYS = map[string]string{
+					"destkey": "field",
+				}
+
+				return c
+			},
+		},
+		// Sender github
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_SENDERS_GITHUB_CREDENTIALS_OWNER", "user")
+				setenv("CHYLE_SENDERS_GITHUB_CREDENTIALS_OAUTHTOKEN", "d41d8cd98f00b204e9800998ecf8427e")
+				setenv("CHYLE_SENDERS_GITHUB_RELEASE_TEMPLATE", "{{.}}")
+				setenv("CHYLE_SENDERS_GITHUB_RELEASE_TAGNAME", "v2.0.0")
+				setenv("CHYLE_SENDERS_GITHUB_REPOSITORY_NAME", "test")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASSENDERS = true
+				c.FEATURES.HASGITHUBRELEASESENDER = true
+				c.SENDERS.GITHUB.CREDENTIALS.OAUTHTOKEN = "d41d8cd98f00b204e9800998ecf8427e"
+				c.SENDERS.GITHUB.CREDENTIALS.OWNER = "user"
+				c.SENDERS.GITHUB.RELEASE.TAGNAME = "v2.0.0"
+				c.SENDERS.GITHUB.RELEASE.TEMPLATE = "{{.}}"
+				c.SENDERS.GITHUB.REPOSITORY.NAME = "test"
+
+				return c
+			},
+		},
+		// Sender stdout
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_SENDERS_STDOUT_FORMAT", "template")
+				setenv("CHYLE_SENDERS_STDOUT_TEMPLATE", "{{.}}")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASSTDOUTSENDER = true
+				c.FEATURES.HASSENDERS = true
+				c.SENDERS.STDOUT.FORMAT = "template"
+				c.SENDERS.STDOUT.TEMPLATE = "{{.}}"
+
+				return c
+			},
+		},
+		{
+			func() {
+				setenv("CHYLE_GIT_REPOSITORY_PATH", "test")
+				setenv("CHYLE_GIT_REFERENCE_FROM", "v1.0.0")
+				setenv("CHYLE_GIT_REFERENCE_TO", "v2.0.0")
+				setenv("CHYLE_SENDERS_STDOUT_FORMAT", "json")
+			},
+			func() CHYLE {
+				c := CHYLE{}
+				c.GIT.REPOSITORY.PATH = "test"
+				c.GIT.REFERENCE.FROM = "v1.0.0"
+				c.GIT.REFERENCE.TO = "v2.0.0"
+				c.FEATURES.HASSTDOUTSENDER = true
+				c.FEATURES.HASSENDERS = true
+				c.SENDERS.STDOUT.FORMAT = "json"
+
+				return c
+			},
+		},
+	}
+
+	for _, test := range tests {
+		restoreEnvs()
+		chyleConfig = CHYLE{}
+		test.f()
+
+		config, err := envh.NewEnvTree("^CHYLE", "_")
+
+		assert.NoError(t, err)
+
+		err = resolveConfig(&config)
+
+		assert.NoError(t, err)
+
+		assert.Equal(t, test.c(), chyleConfig)
 	}
 }
 
