5bfd870ecba81a6765c858ce95607a3953347264
antham <hamonanth@gmail.com>
2017-05-11 13:02:11 +0200

    fix(chyle/extractor_regexp) : set only "true" and "false" as boolean

---
+++ chyle/converter.go
@@ -0,0 +1,17 @@
+package chyle
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func parseBool(str string) (bool, error) {
+	b, err := strconv.ParseBool(str)
+
+	switch str {
+	case "1", "t", "T", "TRUE", "True", "0", "f", "F", "FALSE", "False":
+		return false, fmt.Errorf("Can't convert %s to boolean", str)
+	}
+
+	return b, err
+}
