aeedebeafa55e5c4a638d63f98036e7b6393bef5
antham <hamonanth@gmail.com>
2017-05-12 15:52:33 +0200

    fic(chyle/decorator) : don't throw an error on 404

--- chyle/decorator_jira_issue_test.go
+++ chyle/decorator_jira_issue_test.go
@@ -22,11 +22,11 @@ func TestJiraDecorator(t *testing.T) {
 
 	gock.New("http://test.com/rest/api/2/issue/10000").
 		Reply(200).
-		BodyString(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
+		JSON(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
 
 	gock.New("http://test.com/rest/api/2/issue/EX-1").
 		Reply(200).
-		BodyString(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
+		JSON(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
 
 	client := &http.Client{Transport: &http.Transport{}}
 	gock.InterceptClient(client)
@@ -66,7 +66,7 @@ func TestJiraDecoratorWithNoJiraIssueIdDefined(t *testing.T) {
 
 	gock.New("http://test.com/rest/api/2/issue/10000").
 		Reply(200).
-		BodyString(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
+		JSON(`{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"10000","self":"http://test.com/jira/rest/api/2/issue/10000","key":"EX-1","names":{"watcher":"watcher","attachment":"attachment","sub-tasks":"sub-tasks","description":"description","project":"project","comment":"comment","issuelinks":"issuelinks","worklog":"worklog","updated":"updated","timetracking":"timetracking"	}}`)
 
 	client := &http.Client{Transport: &http.Transport{}}
 	gock.InterceptClient(client)
@@ -82,4 +82,37 @@ func TestJiraDecoratorWithNoJiraIssueIdDefined(t *testing.T) {
 	assert.NoError(t, err, "Must return no errors")
 	assert.Equal(t, expected, *result, "Must return same struct than the one submitted")
 	assert.False(t, gock.IsDone(), "Must have one pending request")
+}
+
+func TestJiraDecoratorWhenIssueIsNotFound(t *testing.T) {
+	chyleConfig = CHYLE{}
+	chyleConfig.DECORATORS.JIRA.KEYS = map[string]string{}
+	chyleConfig.FEATURES.HASJIRADECORATOR = true
+	chyleConfig.DECORATORS.JIRA.CREDENTIALS.USERNAME = "test"
+	chyleConfig.DECORATORS.JIRA.CREDENTIALS.PASSWORD = "test"
+	chyleConfig.DECORATORS.JIRA.CREDENTIALS.URL = "http://test.com"
+	chyleConfig.DECORATORS.JIRA.KEYS["jiraIssueKey"] = "key"
+	chyleConfig.DECORATORS.JIRA.KEYS["whatever"] = "whatever"
+
+	defer gock.Off()
+
+	gock.New("http://test.com/rest/api/2/issue/10000").
+		Reply(404).
+		JSON(`{"errorMessages":["Issue does not exist or you do not have permission to see it."],"errors":{}}`)
+
+	client := &http.Client{Transport: &http.Transport{}}
+	gock.InterceptClient(client)
+
+	j := jiraIssueDecorator{*client}
+
+	result, err := j.decorate(&map[string]interface{}{"test": "test", "jiraIssueId": int64(10000)})
+
+	expected := map[string]interface{}{
+		"test":        "test",
+		"jiraIssueId": int64(10000),
+	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, *result)
+	assert.True(t, gock.IsDone(), "Must have no pending requests")
 }
