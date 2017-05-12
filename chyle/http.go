0e14a47c98295380a1061a44aea1856eb2f9a830
antham <hamonanth@gmail.com>
2017-05-12 15:48:04 +0200

    ref(chyle/http) : remove useless error add generic http error

--- chyle/http.go
+++ chyle/http.go
@@ -5,30 +5,18 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 )
 
-// ErrCantReadHTTPResponse is triggered when reading http
-// response body failed
-type ErrCantReadHTTPResponse struct {
-	URL *url.URL
+// ErrResponse is triggered when status code is greater or equal to 400
+type ErrResponse struct {
+	request  *http.Request
+	response *http.Response
+	body     []byte
 }
 
 // Error output error as string
-func (e ErrCantReadHTTPResponse) Error() string {
-	return fmt.Sprintf("can't read http response from %s", e.URL)
-}
-
-// ErrWrongHTTPStatusCode is triggered when status code is greater or equal to
-// 400
-type ErrWrongHTTPStatusCode struct {
-	request *http.Request
-	body    []byte
-}
-
-// Error output error as string
-func (e ErrWrongHTTPStatusCode) Error() string {
-	return fmt.Sprintf("an error occured when contacting remote api through %s, status code %d, body %s", e.request.URL, e.request.Response.StatusCode, e.body)
+func (e ErrResponse) Error() string {
+	return fmt.Sprintf("an error occured when contacting remote api through %s, status code %d, body %s", e.request.URL, e.response.StatusCode, e.body)
 }
 
 // setHeaders setup headers on request from a map header key -> header value
@@ -38,32 +26,31 @@ func setHeaders(request *http.Request, headers map[string]string) {
 	}
 }
 
-// sendRequest picks a request and send it with given client and handle all error
-// boilerplate and return status code and body as byte slice
+// sendRequest picks a request and send it with given client
 func sendRequest(client *http.Client, request *http.Request) (int, []byte, error) {
-	rep, err := client.Do(request)
+	response, err := client.Do(request)
 
 	if err != nil {
-		return 0, nil, err
+		return 0, []byte{}, err
 	}
 
 	defer func() {
-		err = rep.Body.Close()
+		err = response.Body.Close()
 
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
-	b, err := ioutil.ReadAll(rep.Body)
+	b, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-		return 0, nil, ErrCantReadHTTPResponse{request.URL}
+		return response.StatusCode, b, ErrResponse{request, response, b}
 	}
 
-	if rep.StatusCode >= 400 {
-		return rep.StatusCode, b, ErrWrongHTTPStatusCode{request, b}
+	if response.StatusCode >= 400 {
+		return response.StatusCode, b, ErrResponse{request, response, b}
 	}
 
-	return rep.StatusCode, b, nil
+	return response.StatusCode, b, nil
 }
