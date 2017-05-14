6e27c10761c0422516927cb4f5e8693b8055c5c4
antham <hamonanth@gmail.com>
2017-05-14 15:15:12 +0200

    ref(chyle/process) : rewrite

--- chyle/process.go
+++ chyle/process.go
@@ -1,10 +1,10 @@
 package chyle
 
 import (
-	pkgDecorators "github.com/antham/chyle/chyle/decorators"
-	pkgExtractors "github.com/antham/chyle/chyle/extractors"
-	pkgMatchers "github.com/antham/chyle/chyle/matchers"
-	pkgSenders "github.com/antham/chyle/chyle/senders"
+	"github.com/antham/chyle/chyle/decorators"
+	"github.com/antham/chyle/chyle/extractors"
+	"github.com/antham/chyle/chyle/matchers"
+	"github.com/antham/chyle/chyle/senders"
 
 	"srcd.works/go-git.v4/plumbing/object"
 )
@@ -12,52 +12,49 @@ import (
 // process represents all configuration operations defined
 // needed to create a changelog
 type process struct {
-	matchers   *[]pkgMatchers.Matcher
-	extractors *[]pkgExtractors.Extracter
-	decorators *map[string][]pkgDecorators.Decorater
-	senders    *[]pkgSenders.Sender
+	matchers   *[]matchers.Matcher
+	extractors *[]extractors.Extracter
+	decorators *map[string][]decorators.Decorater
+	senders    *[]senders.Sender
 }
 
 // buildProcess creates process entity from defined configuration
 func buildProcess() *process {
-	matchers := &[]pkgMatchers.Matcher{}
-	extractors := &[]pkgExtractors.Extracter{}
-	decorators := &map[string][]pkgDecorators.Decorater{}
-	senders := &[]pkgSenders.Sender{}
+	p := &process{
+		&[]matchers.Matcher{},
+		&[]extractors.Extracter{},
+		&map[string][]decorators.Decorater{},
+		&[]senders.Sender{},
+	}
 
 	if chyleConfig.FEATURES.HASMATCHERS {
-		matchers = pkgMatchers.CreateMatchers(chyleConfig.MATCHERS)
+		p.matchers = matchers.CreateMatchers(chyleConfig.MATCHERS)
 	}
 
 	if chyleConfig.FEATURES.HASEXTRACTORS {
-		extractors = pkgExtractors.CreateExtractors(chyleConfig.EXTRACTORS)
+		p.extractors = extractors.CreateExtractors(chyleConfig.EXTRACTORS)
 	}
 
 	if chyleConfig.FEATURES.HASDECORATORS {
-		decorators = pkgDecorators.CreateDecorators(map[string]bool{"jiraIssueDecorator": chyleConfig.FEATURES.HASJIRAISSUEDECORATOR, "githubIssueDecorator": chyleConfig.FEATURES.HASGITHUBISSUEDECORATOR, "envDecorator": chyleConfig.FEATURES.HASENVDECORATOR}, chyleConfig.DECORATORS)
+		p.decorators = decorators.CreateDecorators(map[string]bool{"jiraIssueDecorator": chyleConfig.FEATURES.HASJIRAISSUEDECORATOR, "githubIssueDecorator": chyleConfig.FEATURES.HASGITHUBISSUEDECORATOR, "envDecorator": chyleConfig.FEATURES.HASENVDECORATOR}, chyleConfig.DECORATORS)
 	}
 
 	if chyleConfig.FEATURES.HASSENDERS {
-		senders = pkgSenders.CreateSenders(map[string]bool{"githubReleaseSender": chyleConfig.FEATURES.HASGITHUBRELEASESENDER, "stdoutSender": chyleConfig.FEATURES.HASSTDOUTSENDER}, chyleConfig.SENDERS)
+		p.senders = senders.CreateSenders(map[string]bool{"githubReleaseSender": chyleConfig.FEATURES.HASGITHUBRELEASESENDER, "stdoutSender": chyleConfig.FEATURES.HASSTDOUTSENDER}, chyleConfig.SENDERS)
 	}
 
-	return &process{
-		matchers,
-		extractors,
-		decorators,
-		senders,
-	}
+	return p
 }
 
 // proceed extracts datas from a set of commits
 func proceed(process *process, commits *[]object.Commit) error {
-	changelog := pkgExtractors.Extract(process.extractors, pkgMatchers.Filter(process.matchers, commits))
+	changelog := extractors.Extract(process.extractors, matchers.Filter(process.matchers, commits))
 
-	changelog, err := pkgDecorators.Decorate(process.decorators, changelog)
+	changelog, err := decorators.Decorate(process.decorators, changelog)
 
 	if err != nil {
 		return err
 	}
 
-	return pkgSenders.Send(process.senders, changelog)
+	return senders.Send(process.senders, changelog)
 }
