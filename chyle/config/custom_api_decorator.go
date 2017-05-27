5dcec9c33f42830a5d862387013405086037b59a
antham <hamonanth@gmail.com>
2017-05-27 23:06:19 +0200

    ref(config/custom_api_decorator) : split in sub functions

--- chyle/config/custom_api_decorator.go
+++ chyle/config/custom_api_decorator.go
@@ -8,6 +8,51 @@ import (
 	"github.com/antham/envh"
 )
 
+func getCustomAPIMandatoryParamsRefs() []struct {
+	ref      *string
+	keyChain []string
+} {
+	return []struct {
+		ref      *string
+		keyChain []string
+	}{
+		{
+			&chyleConfig.DECORATORS.CUSTOMAPI.ENDPOINT.URL,
+			[]string{"CHYLE", "DECORATORS", "CUSTOMAPI", "ENDPOINT", "URL"},
+		},
+		{
+			&chyleConfig.DECORATORS.CUSTOMAPI.CREDENTIALS.TOKEN,
+			[]string{"CHYLE", "DECORATORS", "CUSTOMAPI", "CREDENTIALS", "TOKEN"},
+		},
+	}
+}
+
+func getCustomAPIFeatureRefs() []*bool {
+	return []*bool{
+		&chyleConfig.FEATURES.DECORATORS.ENABLED,
+		&chyleConfig.FEATURES.DECORATORS.CUSTOMAPI,
+	}
+}
+
+func getCustomAPICustomValidationFuncs(config *envh.EnvTree) []func() error {
+	return []func() error{
+		func() error {
+			keyChain := []string{"CHYLE", "DECORATORS", "CUSTOMAPI", "ENDPOINT", "URL"}
+			URL := config.FindStringUnsecured(keyChain...)
+
+			if !regexp.MustCompile(`{{\s*ID\s*}}`).MatchString(URL) {
+				return fmt.Errorf(`ensure you defined a placeholder {{ID}} in URL defined in "%s"`, strings.Join(keyChain, "_"))
+			}
+
+			return nil
+		},
+	}
+}
+
+func getCustomAPICustomSettersFuncs() []func(*CHYLE) {
+	return []func(*CHYLE){}
+}
+
 // customAPIDecoratorConfigurator creates a custom api configurater from apiDecoratorConfigurator
 func customAPIDecoratorConfigurator(config *envh.EnvTree) configurater {
 	return &apiDecoratorConfigurator{
@@ -16,36 +61,10 @@ func customAPIDecoratorConfigurator(config *envh.EnvTree) configurater {
 			"CUSTOMAPIID",
 			"CUSTOMAPI",
 			&chyleConfig.DECORATORS.CUSTOMAPI.KEYS,
-			[]struct {
-				ref      *string
-				keyChain []string
-			}{
-				{
-					&chyleConfig.DECORATORS.CUSTOMAPI.ENDPOINT.URL,
-					[]string{"CHYLE", "DECORATORS", "CUSTOMAPI", "ENDPOINT", "URL"},
-				},
-				{
-					&chyleConfig.DECORATORS.CUSTOMAPI.CREDENTIALS.TOKEN,
-					[]string{"CHYLE", "DECORATORS", "CUSTOMAPI", "CREDENTIALS", "TOKEN"},
-				},
-			},
-			[]*bool{
-				&chyleConfig.FEATURES.DECORATORS.ENABLED,
-				&chyleConfig.FEATURES.DECORATORS.CUSTOMAPI,
-			},
-			[]func() error{
-				func() error {
-					keyChain := []string{"CHYLE", "DECORATORS", "CUSTOMAPI", "ENDPOINT", "URL"}
-					URL := config.FindStringUnsecured(keyChain...)
-
-					if !regexp.MustCompile(`{{\s*ID\s*}}`).MatchString(URL) {
-						return fmt.Errorf(`ensure you defined a placeholder {{ID}} in URL defined in "%s"`, strings.Join(keyChain, "_"))
-					}
-
-					return nil
-				},
-			},
-			[]func(*CHYLE){},
+			getCustomAPIMandatoryParamsRefs(),
+			getCustomAPIFeatureRefs(),
+			getCustomAPICustomValidationFuncs(config),
+			getCustomAPICustomSettersFuncs(),
 		},
 	}
 }
