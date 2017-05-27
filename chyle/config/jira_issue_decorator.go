1447eaa0c445c8891e97832dc0d0ed2b6fec752d
antham <hamonanth@gmail.com>
2017-05-28 00:36:27 +0200

    ref(config/*decorator) : split in sub functions, renaming

--- chyle/config/jira_issue_decorator.go
+++ chyle/config/jira_issue_decorator.go
@@ -4,6 +4,44 @@ import (
 	"github.com/antham/envh"
 )
 
+func getJiraIssueDecoratorMandatoryParamsRefs() []struct {
+	ref      *string
+	keyChain []string
+} {
+	return []struct {
+		ref      *string
+		keyChain []string
+	}{
+		{
+			&chyleConfig.DECORATORS.JIRAISSUE.CREDENTIALS.URL,
+			[]string{"CHYLE", "DECORATORS", "JIRAISSUE", "CREDENTIALS", "URL"},
+		},
+		{
+			&chyleConfig.DECORATORS.JIRAISSUE.CREDENTIALS.USERNAME,
+			[]string{"CHYLE", "DECORATORS", "JIRAISSUE", "CREDENTIALS", "USERNAME"},
+		},
+		{
+			&chyleConfig.DECORATORS.JIRAISSUE.CREDENTIALS.PASSWORD,
+			[]string{"CHYLE", "DECORATORS", "JIRAISSUE", "CREDENTIALS", "PASSWORD"},
+		},
+	}
+}
+
+func getJiraIssueDecoratorFeatureRefs() []*bool {
+	return []*bool{
+		&chyleConfig.FEATURES.DECORATORS.ENABLED,
+		&chyleConfig.FEATURES.DECORATORS.JIRAISSUE,
+	}
+}
+
+func getJiraIssueDecoratorCustomValidationFuncs(config *envh.EnvTree) []func() error {
+	return []func() error{}
+}
+
+func getJiraIssueDecoratorCustomSettersFuncs() []func(*CHYLE) {
+	return []func(*CHYLE){}
+}
+
 // jiraIssueDecoratorConfigurator creates a jira configurater from apiDecoratorConfigurator
 func jiraIssueDecoratorConfigurator(config *envh.EnvTree) configurater {
 	return &apiDecoratorConfigurator{
@@ -13,29 +51,10 @@ func jiraIssueDecoratorConfigurator(config *envh.EnvTree) configurater {
 			"jiraIssueId",
 			"JIRAISSUE",
 			&chyleConfig.DECORATORS.JIRAISSUE.KEYS,
-			[]struct {
-				ref      *string
-				keyChain []string
-			}{
-				{
-					&chyleConfig.DECORATORS.JIRAISSUE.CREDENTIALS.URL,
-					[]string{"CHYLE", "DECORATORS", "JIRAISSUE", "CREDENTIALS", "URL"},
-				},
-				{
-					&chyleConfig.DECORATORS.JIRAISSUE.CREDENTIALS.USERNAME,
-					[]string{"CHYLE", "DECORATORS", "JIRAISSUE", "CREDENTIALS", "USERNAME"},
-				},
-				{
-					&chyleConfig.DECORATORS.JIRAISSUE.CREDENTIALS.PASSWORD,
-					[]string{"CHYLE", "DECORATORS", "JIRAISSUE", "CREDENTIALS", "PASSWORD"},
-				},
-			},
-			[]*bool{
-				&chyleConfig.FEATURES.DECORATORS.ENABLED,
-				&chyleConfig.FEATURES.DECORATORS.JIRAISSUE,
-			},
-			[]func() error{},
-			[]func(*CHYLE){},
+			getJiraIssueDecoratorMandatoryParamsRefs(),
+			getJiraIssueDecoratorFeatureRefs(),
+			getJiraIssueDecoratorCustomValidationFuncs(config),
+			getJiraIssueDecoratorCustomSettersFuncs(),
 		},
 	}
 }
