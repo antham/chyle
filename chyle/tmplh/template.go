67d061e593d177259b4afa5273d2b4d98cecbd18
antham <hamonanth@gmail.com>
2017-05-29 01:10:44 +0200

    ref(tmplh/template) : extract Parse from Buil
    * remove error use errh decorator function instead
    * extract Parse for validity checks purpose

--- chyle/tmplh/template.go
+++ chyle/tmplh/template.go
@@ -2,22 +2,12 @@ package tmplh
 
 import (
 	"bytes"
-	"fmt"
 	tmpl "html/template"
 
 	"github.com/Masterminds/sprig"
+
+	"github.com/antham/chyle/chyle/errh"
 )
-
-// ErrTemplateMalformed issued when something wrong
-// happened when creation a template
-type ErrTemplateMalformed struct {
-	err error
-}
-
-// Error return string error
-func (e ErrTemplateMalformed) Error() string {
-	return fmt.Sprintf("check your template is well-formed : %s", e.err.Error())
-}
 
 var store = map[string]interface{}{}
 
@@ -37,17 +27,23 @@ func get(key string) interface{} {
 	return store[key]
 }
 
-// Build returns, using a template and generic data, an executed template
-func Build(ID string, template string, data interface{}) (string, error) {
+// Parse creates a template instance from string template
+func Parse(ID string, template string) (*tmpl.Template, error) {
 	funcMap := sprig.FuncMap()
 	funcMap["isset"] = isset
 	funcMap["set"] = set
 	funcMap["get"] = get
 
-	t, err := tmpl.New(ID).Funcs(funcMap).Parse(template)
+	return tmpl.New(ID).Funcs(funcMap).Parse(template)
+}
+
+// Build creates a template instance and runs it against datas to get
+// final string
+func Build(ID string, template string, data interface{}) (string, error) {
+	t, err := Parse(ID, template)
 
 	if err != nil {
-		return "", ErrTemplateMalformed{err}
+		return "", errh.AddCustomMessageToError("check your template is well-formed", err)
 	}
 
 	b := bytes.Buffer{}
