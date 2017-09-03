9ffbc9303d3219909279d6e5ead1b3c164318d99
antham <hamonanth@gmail.com>
2017-09-03 11:34:00 +0200

    feat(prompt) : add newGroupEnvPromptWithCounter

--- prompt/generic_prompt_test.go
+++ prompt/generic_prompt_test.go
@@ -59,5 +59,83 @@ func TestNewEnvPromptWithEmptyValueGiven(t *testing.T) {
 }
 
 func TestNewGroupEnvPromptWithCounter(t *testing.T) {
+	store := &Store{}
 
+	var stdout bytes.Buffer
+	buf := "test0\ntest1\n1\ntest2\ntest3\nq\n"
+	prompts := newGroupEnvPromptWithCounter(
+		[]envConfig{
+			envConfig{"TEST_0", "TEST_1", "TEST_*_0", "Enter a value"},
+			envConfig{"TEST_1", "choice", "TEST_*_1", "Enter a value"},
+		}, store)
+
+	var choice = []strumt.Prompter{
+		&switchPrompt{
+			"choice",
+			[]switchChoice{
+				switchChoice{
+					"1", "Add new test values", "TEST_0",
+				},
+				switchChoice{
+					"q", "Quit", "",
+				},
+			},
+		},
+	}
+
+	prompts = append(prompts, choice...)
+
+	s := strumt.NewPromptsFromReaderAndWriter(bytes.NewBufferString(buf), &stdout)
+
+	for _, item := range prompts {
+		switch prompt := item.(type) {
+		case strumt.LinePrompter:
+			s.AddLinePrompter(prompt)
+		case strumt.MultilinePrompter:
+			s.AddMultilinePrompter(prompt)
+		}
+	}
+
+	s.SetFirst("TEST_0")
+	s.Run()
+
+	scenario := s.Scenario()
+
+	steps := []struct {
+		input string
+		err   error
+	}{
+		{
+			"test0",
+			nil,
+		},
+		{
+			"test1",
+			nil,
+		},
+		{
+			"1",
+			nil,
+		},
+		{
+			"test2",
+			nil,
+		},
+		{
+			"test3",
+			nil,
+		},
+		{
+			"q",
+			nil,
+		},
+	}
+
+	for i, step := range steps {
+		assert.Nil(t, step.err)
+		assert.Len(t, scenario[i].Inputs(), 1)
+		assert.Equal(t, scenario[i].Inputs()[0], step.input)
+	}
+
+	assert.Equal(t, &Store{"TEST_0_0": "test0", "TEST_0_1": "test1", "TEST_1_0": "test2", "TEST_1_1": "test3"}, store)
 }
