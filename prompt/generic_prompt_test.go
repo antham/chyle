760349c126dd4d0028612831e7b51dc844fd0f3e
antham <hamonanth@gmail.com>
2017-09-04 21:24:38 +0200

    feat(prompt) : add newPromptWithCustomHandlers

--- prompt/generic_prompt_test.go
+++ prompt/generic_prompt_test.go
@@ -2,6 +2,7 @@ package prompt
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/antham/strumt"
@@ -175,4 +176,50 @@ func TestNewEnvPrompts(t *testing.T) {
 	assert.Nil(t, scenario[1].Error())
 
 	assert.Equal(t, &Store{"TEST_PROMPT_1": "1", "TEST_PROMPT_2": "2"}, store)
+}
+
+func TestNewPromptWithCustomHandlers(t *testing.T) {
+	store := &Store{}
+
+	var stdout bytes.Buffer
+	buf := "wrong\nright\n"
+
+	p := newPromptWithCustomHandlers(
+		envConfig{"test", "", "TEST", "Enter a value"},
+		func(val string) string {
+			return ""
+		},
+		func(err error) string {
+			return "test"
+		},
+		func(val string) error {
+			if val != "right" {
+				return fmt.Errorf(`Value must be right`)
+			}
+
+			(*store)["TEST"] = val
+
+			return nil
+		},
+		store,
+	)
+
+	s := strumt.NewPromptsFromReaderAndWriter(bytes.NewBufferString(buf), &stdout)
+	s.AddLinePrompter(p.(strumt.LinePrompter))
+	s.SetFirst("test")
+	s.Run()
+
+	scenario := s.Scenario()
+
+	assert.Len(t, scenario, 2)
+	assert.Equal(t, scenario[0].PromptString(), "Enter a value")
+	assert.Len(t, scenario[0].Inputs(), 1)
+	assert.Equal(t, scenario[0].Inputs()[0], "wrong")
+	assert.EqualError(t, scenario[0].Error(), "Value must be right")
+	assert.Equal(t, scenario[1].PromptString(), "Enter a value")
+	assert.Len(t, scenario[1].Inputs(), 1)
+	assert.Equal(t, scenario[1].Inputs()[0], "right")
+	assert.Nil(t, scenario[1].Error())
+
+	assert.Equal(t, &Store{"TEST": "right"}, store)
 }
