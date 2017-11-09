6f602c53645dbbf46f6e64eac0331a1beaad23da
antham <hamonanth@gmail.com>
2017-11-09 23:36:50 +0100

    fix(cmd/ui) : remove unused function

--- cmd/ui.go
+++ cmd/ui.go
@@ -1,15 +1,9 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/fatih/color"
 )
 
 var failure = func(err error) {
 	color.Red(err.Error())
-}
-
-var println = func(data string) {
-	fmt.Println(data)
 }
