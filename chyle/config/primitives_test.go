e50bbd70af5d76048788109bf4dbdea4dfbd34f2
antham <hamonanth@gmail.com>
2017-12-13 23:31:40 +0100

    test(config/primitives) : add tests

---
+++ chyle/config/primitives_test.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMissingEnvError(t *testing.T) {
+	e := MissingEnvError{envs: []string{"TEST"}}
+
+	assert.Equal(t, `environment variable missing : "TEST"`, e.Error())
+
+	e = MissingEnvError{envs: []string{"TEST", "TEST1"}}
+
+	assert.Equal(t, `environments variables missing : "TEST", "TEST1"`, e.Error())
+}
